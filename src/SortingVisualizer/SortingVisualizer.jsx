eefc0aefdd2fe71398528dc61ea653c24ed98914
vietanh2810 <vietanhc12@gmail.com>
2023-01-02 12:37:56 +0100

    feat: adding bar representation for element and hook

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./SortingVisualizer.css";
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {    
+        super(props);
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < 310; i++) {
+            array.push(this.randomIntFromInterval(5, 730));
+        }
+        this.setState({array});
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <div className="array-container">
+                {array.map((value, idx) => (
+                    <div 
+                        className="array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    randomIntFromInterval(min, max) { // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
